62bfa6ef06cc3a14eabf7e8a48b019516e5fffdc
Yuxuan Zhang <kate900115@gmail.com>
2018-05-29 15:45:51 +0800

    modified:   ../cpu_module/ioctlrw.c

--- cpu_module/ioctlrw.c
+++ cpu_module/ioctlrw.c
@@ -48,7 +48,7 @@ struct savedAddress savedPhysAddr(uint64_t addr, int op, bool isRead ){
 
 	static int times = 0;
 	times++;
-	pr_info("[savedPhysAddr] the function is called %d times\n",times);
+	pr_info("[savedPhysAddr]\t the function is called %d times\n",times);
 
 	static struct savedAddress SavedAddr;
 
@@ -60,18 +60,13 @@ struct savedAddress savedPhysAddr(uint64_t addr, int op, bool isRead ){
 		SavedAddr.op = op;
 		return SavedAddr;
 	}
-	//pr_info("saved addr = %ld\n", AddrSaved);
-	//if (isRead) return AddrSaved;
-	//AddrSaved = addr;
-	//pr_info("### the new address %ld is saved\n", AddrSaved);
-	//return 0;
 
 }
 
 
 //zyuxuan
 int ioctl_v2p_convert(unsigned long arg){
-	pr_info("[ioctl_v2p] I'm ioctl_v2p_convert\n");	
+	pr_info("[ioctl_v2p]\t I'm ioctl_v2p_convert\n");	
 	int error = 0;
 	// to copy the argument from user space to kernel space
 	struct cpuaddr_t addr;
@@ -80,20 +75,16 @@ int ioctl_v2p_convert(unsigned long arg){
 		error = -EFAULT;
 		return error;
 	}
-
-	
-//	void* address = addr.handle;
-//	phys_addr_t paddr = virt_to_phys(address);
 	
 	// read the address from saved address
 	struct savedAddress temp = savedPhysAddr(0,0,1);
 	addr.paddr = temp.addr;
 
- 	pr_info("[ioctl_v2p] previously saved physical address = %ld\n", addr.paddr);
+ 	pr_info("[ioctl_v2p]\t previously saved physical address = %ld\n", addr.paddr);
 	
 	savedPhysAddr(0,0,0);
 
-	pr_info("[ioctl_v2p] write 0 to saved address\n");
+	pr_info("[ioctl_v2p]\t write 0 to saved address\n");
 
 	if (copy_to_user((void*)arg, &addr, sizeof(struct cpuaddr_t))){
 		printk(KERN_ERR"%s(): Error in copy_from_user()\n",__FUNCTION__);
@@ -106,7 +97,7 @@ int ioctl_v2p_convert(unsigned long arg){
 
 //zyuxuan
 int ioctl_p2v_convert(unsigned long arg){
-	pr_info("[ioctl_p2v] I'm ioctl_p2v_convert\n");	
+	pr_info("[ioctl_p2v]\t I'm ioctl_p2v_convert\n");	
 	int error = 0;
 	// to copy the argument from user space to kernel space
 	struct cpuaddr_t addr;
@@ -116,13 +107,8 @@ int ioctl_p2v_convert(unsigned long arg){
 		return error;
 	}
 
-
 	savedPhysAddr(addr.paddr, 1, 0);
- 	pr_info("[ioctl_p2v] now is saving physical address = %ld\n", addr.paddr);
-	
-	
-
-
+ 	pr_info("[ioctl_p2v]\t now is saving physical address = %ld\n", addr.paddr);
 
 	return error;
 }
