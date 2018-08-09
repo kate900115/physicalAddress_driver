5895346b2905bad8ea063702456d99fee87c2369
Yuxuan Zhang <kate900115@gmail.com>
2018-08-09 15:52:50 +0800

    modified:   ioctlrw.c 	modified:   v2p2vdrv.c

--- v2p2v/ioctlrw.c
+++ v2p2v/ioctlrw.c
@@ -13,14 +13,58 @@
 #include "v2p2vdrv.h"
 #include "v2p2vioctl.h"
 
-//-----------------------------------------------------------------------------
-
+	// savePhysAddr() is a function that saved physcial 	//
+	// address which is passed from ioctl()			//
 
 struct savedAddress savedPhysAddr(uint64_t addr, int op, bool isRead ){
 
+	// for debug.						//
+	// it shows how many times the savePhysAddr() is called.//
+	
+	#ifdef DEBUG
 	static int times = 0;
 	times++;
 	pr_info("[savedPhysAddr] the function is called %d times\n",times);
+	#endif
+
+	// first need to figure out the type of operation:	//	
+	// (1) read or (2) write				//
+	// 							//
+	// (1) If it is a read operation:			//
+	//     a. It means that in user space, the programmer	// 
+	//        want to convert physical address to virtual	// 
+	//        address. The user calls mmap(), and mmap()	// 
+	//        invokes savedPhysAddr() just to get the	// 
+	//        physical address saved by ioctl().		//
+	//     b. It means that in user space, the programmer 	//
+	//        want to convert virtual address to physical	// 
+	//        address. The user calls ioctl(), and ioctl()	//
+	//        invokes savedPhysAddr() to get the physical	// 
+	//        address saved by mmap(), and then return it	// 
+	//        back to user space.				//
+	//							//
+	//							//
+	// (2) If it is a write operation:			//
+	//     a. It means that in user space, the programmer	// 
+	//        want to convert virtual address to physical	//
+	//        address. The user called mmap(), and mmap()	// 
+	//        calls savedPhysAddr() to store the converted	//
+	//        phsical address into a static variable 	//
+	//        called in "SavedAddr" in kernel space.	//
+	//        Later on, the ioctl() called by user will 	//
+	//        read the "SavedAddr" to get the physical 	//
+	//        address.					//
+	//     b. It means that in user space, the programmer	//
+	//        want to convert physical address to virtual	//
+	//        address. The user calls ioctl(), and ioctl()	//
+	//        calls the savedPhysAddr() to store the 	//
+	//        physical address into a static variable 	//
+	//        called "savedAddr" in kernel space. Later on,	//
+	//        the mmap() called by user will read the 	//
+	//        "SavedAddr" in kernel space, and convert it 	//
+	//        into kernel space virtual pointer, and then	//
+	//        send it back to user space.   		//
+	
 
 	static struct savedAddress SavedAddr;
 
@@ -36,11 +80,26 @@ struct savedAddress savedPhysAddr(uint64_t addr, int op, bool isRead ){
 }
 
 
-//zyuxuan
+
 int ioctl_v2p_convert(unsigned long arg){
-	pr_info("[ioctl_v2p] I'm ioctl_v2p_convert\n");	
+
+	// Before kernel calls this function, it has already 	//
+	// called v2p2v_mmap() to allocate virtual pointer to	//
+	// physical pointer and stored that physical pointer 	//
+	// to "savedAddr". User calls v2p2v_ioctl() just to     //
+	// fetch the physical address back to user space.	//
+	//							//
+	// (1) copy user space variable void* arg into kernel	//
+	//     space variable cpuaddr_t addr.			//
+	// (2) read the address from saved address.		//
+	// (3) store the address into struct cpuaddr_t addr	//
+	//     and then clean up the static varialbe 		//
+	//     "savedAddr".					//
+	// (4) copy the kernel space variable cpuaddr_t addr 	//
+	//     into user space variable void* arg.		//
+
 	int error = 0;
-	// to copy the argument from user space to kernel space
+
 	struct cpuaddr_t addr;
 	if (copy_from_user(&addr, (void*)arg, sizeof(struct cpuaddr_t))){
 		printk(KERN_ERR"%s(): Error in copy_from_user()\n", __FUNCTION__);
@@ -48,15 +107,9 @@ int ioctl_v2p_convert(unsigned long arg){
 		return error;
 	}
 	
-	// read the address from saved address
 	struct savedAddress temp = savedPhysAddr(0,0,1);
-	addr.paddr = temp.addr;
-
- 	pr_info("[ioctl_v2p] previously saved physical address = %ld\n", addr.paddr);
-	
+	addr.paddr = temp.addr;	
 	savedPhysAddr(0,0,0);
-
-	pr_info("[ioctl_v2p] write 0 to saved address\n");
 
 	if (copy_to_user((void*)arg, &addr, sizeof(struct cpuaddr_t))){
 		printk(KERN_ERR"%s(): Error in copy_from_user()\n",__FUNCTION__);
@@ -64,14 +117,32 @@ int ioctl_v2p_convert(unsigned long arg){
 		return error;
 	}
 
+	// for debug
+	#ifdef DEBUG
+	pr_info("[ioctl_v2p] I'm ioctl_v2p_convert\n");		
+ 	pr_info("[ioctl_v2p] previously saved physical address = %ld\n", addr.paddr);
+	pr_info("[ioctl_v2p] write 0 to saved address\n");
+	#endif
+	
 	return error;
 }
 
-//zyuxuan
+
 int ioctl_p2v_convert(unsigned long arg){
-	pr_info("[ioctl_p2v] I'm ioctl_p2v_convert\n");	
+
+	// User calls this v2p2v_ioctl() to send the physical 	//
+	// to kernel space for v2p2v_mmap() to convert it into	//
+	// virtual address.					//
+	//							//
+	// (1) copy user space variable void* arg into kernel	//
+	//     space variable cpuaddr_t addr.			//
+	// (2) store the address into struct cpuaddr_t addr	//
+	//     and then clean up the static varialbe 		//
+	//     "savedAddr".					//
+	//     The second argument of savedPhysAddr() indicates	//
+	//     the saved address is used for p2v convertion.	// 
+
 	int error = 0;
-	// to copy the argument from user space to kernel space
 	struct cpuaddr_t addr;
 	if (copy_from_user(&addr, (void*)arg, sizeof(struct cpuaddr_t))){
 		printk(KERN_ERR"%s(): Error in copy_from_user()\n", __FUNCTION__);
@@ -80,7 +151,12 @@ int ioctl_p2v_convert(unsigned long arg){
 	}
 
 	savedPhysAddr(addr.paddr, 1, 0);
+
+	// for debug
+	#ifdef DEBUG
+	pr_info("[ioctl_p2v] I'm ioctl_p2v_convert\n");	
  	pr_info("[ioctl_p2v] now is saving physical address = %d\n", addr.paddr);
+	#endif
 
 	return error;
 }
--- v2p2v/v2p2vdrv.c
+++ v2p2v/v2p2vdrv.c
@@ -35,98 +35,146 @@
 #include <linux/cdev.h>
 
 
-
-//-----------------------------------------------------------------------------
-
 MODULE_AUTHOR("Yuxuan Zhang. v-yuxz@microsoft.com");
 MODULE_LICENSE("GPL");
 
-//-----------------------------------------------------------------------------
+// This module has the following two functionalities:		//
+// 	a) p2v: user calls ioctl() to pass physical addr 	//
+// 		to kernel, and then calls mmap() to get 	//
+// 		the corresponding virtual address.		//
+// 	b) v2p: user calls mmap() to allocate a memory 		//
+// 	        location with a virtual pointer. And then user  //
+// 	        calls ioctl() to get the physical address.	//
+
 static struct gpumem dev;
 
-//-----------------------------------------------------------------------------
-
-static struct gpumem *file_to_device( struct file *file )
-{
+static struct gpumem *file_to_device( struct file *file ){
     return (struct gpumem*)file->private_data;
 }
 
-//--------------------------------------------------------------------
+// file operations includes:					//
+// (1) file open: 						//
+//     static int v2p2v_open(struct inode*, struct file*)	//
+// (2) file close: 						//
+//     static int v2p2v_close(struct inode*, struct file*)	//
+// (3) I/O control:						//
+//     static long v2p2v_ioctl(struct file*, unsigned int,	//
+//     unsigned long)						//
+// (4) mmap: 							//
+//     int v2p2v_mmap(struct file*, vm_struct_area*)		//
+//  								//
+//  these 4 operations will be registered into kernel. 		//
 
-static int v2p2v_open( struct inode *inode, struct file *file )
-{
+static int v2p2v_open( struct inode *inode, struct file *file ){
     file->private_data = (void*)&dev;
     return 0;
 }
 
-//-----------------------------------------------------------------------------
-
-static int v2p2v_close( struct inode *inode, struct file *file )
-{
+static int v2p2v_close( struct inode *inode, struct file *file ){
     file->private_data = 0;
     return 0;
 }
 
-//-----------------------------------------------------------------------------
-
 static long v2p2v_ioctl( struct file *file, unsigned int cmd, unsigned long arg )
 {
-    int error = 0;
-    if (cmd==IOCTL_V2P) {
-	pr_info("[gpumem_ioctl] The ioctl_v2p_convert(arg) is called.\n");
-	error = ioctl_v2p_convert(arg);
-    }
-    if (cmd==IOCTL_P2V){
-	pr_info("[gpumem_ioctl] The ioctl_p2v_convert(arg) is called.\n");
-	error = ioctl_p2v_convert(arg);
-    }
+	// cmd has two operations: (1) IOCTL_V2P & (2) IOCTL_P2V //
+	// (1) IOCTL_V2P: convert virtual address to physical 	 //
+	//     address call ioctl_v2p_convert() and return error //
+	//     code.						 //
+	// (2) IOCTL_P2V: convert physical address to virtual 	 //
+	//     address call ioctl_p2v_convert() and return error //
+	//     code.						 //
 
-    return error;
+	int error = 0;
+	if (cmd==IOCTL_V2P) {
+		#ifdef DEBUG
+		pr_info("[gpumem_ioctl] The ioctl_v2p_convert(arg) is called.\n");
+		#endif
+		error = ioctl_v2p_convert(arg);
+	}	
+	if (cmd==IOCTL_P2V){
+		#ifdef DEBUG
+		pr_info("[gpumem_ioctl] The ioctl_p2v_convert(arg) is called.\n");
+		#endif
+		error = ioctl_p2v_convert(arg);
+	}
+	return error;
 }
 
-//-----------------------------------------------------------------------------
 
 int v2p2v_mmap(struct file *file, struct vm_area_struct *vma)
 {
-
-	pr_info("[mmap] I'm in mmap\n");
-
+	// When mmap() is called with the argument fd = v2p2v,	//
+	// v2p2v_mmap will be called. OS will pass all argument //
+	// to *vma.						//
+	//							//
+	// (1) set size for remap_pfn_range()			//
+	// (2) set page property to be non-cached		//
+	// (3) read saved operation type "savedAddr.op"		//
 	size_t size = vma->vm_end - vma->vm_start;
-
 	if (!(vma->vm_flags & VM_MAYSHARE)){
 		return -EINVAL;
-	}
-	
+	}	
 	vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
 
-	// read value from savedPhysAddr
-	
 	struct savedAddress savedAddr;
 	savedAddr = savedPhysAddr(0, 0, 1);
 
+	// for debug
+	#ifdef DEBUG
+	pr_info("[mmap] I'm in mmap\n");
 	pr_info("[mmap] read previously saved address from kernel\n");
 	pr_info("[mmap] physical address = %ld\n", savedAddr.addr);
+	#endif
+
+	// (1) savedAddr.op == 1: p2v				//			
+	//     This means there is function "ioctl(IOCTL_P2V)"	//
+	//     that calls the savedPhysAddr() to save the 	//
+	//     physical address before this mmap() is called.	//
+	//     In this case, 					//
+	//     a. mmap() uses remap_pfn_range() with physical 	//
+	//        address to be savedPhysAddr to generate a user//
+	//        space virtual address.			//
+	//     b. And then it needs to clean up the value saved //
+	//        in the static variable "savedAddr".		//
+	//							//
+	// (2) savedAddr.op == 0: v2p				//
+	//     This means there is no previous functions that 	//
+	//     calls the savedPhysAddr() to save the physcial 	//
+	//     address.						//
+	//     In this case, 					//
+	//     a. We need to use kmalloc_area() to allocate a 	//
+	//	  virtual address in kernel space. 		//
+	//     b. use virt_to_phys() to convert the kernel space// 
+	//        virtual pointer into physical address.	//
+	//     c. save the physical address into static variable// 
+	//        "savedAddr" for future usage [used by the 	//
+	//        following ioctl() ]				//
+	//     d. use remap_pfn_range(physAddr) to generate 	//
+	//        user space virtual pointer.			//
+	//     e. the 2nd argument for savePhysAddr() is 2, 	//
+	//     	  which indicates that the static variable is 	//
+	//     	  saved by mmap() and waiting for ioctl() to 	//
+	//     	  copy it back to user space.			//
 
 	if (savedAddr.op==1){
-		//p2v
+		#ifdef DEBUG
 		pr_info("[mmap] this is p2v.\n");		
-		//int errorCode = remap_pfn_range(vma, vma->vm_start+savedAddr.addr, savedAddr.addr, size, PAGE_SHARED);  
-		int errorCode = remap_pfn_range(vma, vma->vm_start, savedAddr.addr>>PAGE_SHIFT, size, PAGE_SHARED);  
+		#endif
 
-		// write noop to saved address operation element
+		int errorCode = remap_pfn_range(vma, vma->vm_start, savedAddr.addr>>PAGE_SHIFT, size, PAGE_SHARED);  
 		savedPhysAddr(savedAddr.addr, 0, 0);
-	 
 		if (errorCode){
 			return -ENXIO;
 		}
-
 		return 0;
-	
 	}
 
 	else if (savedAddr.op==0){
-		//v2p
+		#ifdef DEBUG
 		pr_info("[mmap] this is v2p.\n");
+		#endif
+
 		void* kmalloc_area = kmalloc(size, GFP_USER);
 		uint64_t phyaddr = virt_to_phys(kmalloc_area);
 		savedPhysAddr(phyaddr,2,0);
@@ -135,23 +183,20 @@ int v2p2v_mmap(struct file *file, struct vm_area_struct *vma)
 			pr_info("[mmap] kmalloc area failed\n");
 			return -ENXIO;
 		}
-		//int errorCode = remap_pfn_range(vma, vma->vm_start+phyaddr, phyaddr, size, PAGE_SHARED);
+		
 		int errorCode = remap_pfn_range(vma, vma->vm_start, phyaddr>>PAGE_SHIFT, size, PAGE_SHARED);  
-
-
 		if (errorCode){
 			return -ENXIO;
 		}
-
 		return 0;
-
 	}
-
-
 }
 
-//-----------------------------------------------------------------------------
-
+// register the ioctl(), file_open(), 		//
+// file_release(), mmap() functions to kernel. 	//
+// This functions called by user space will be  //
+// replaced by our self-designed functions.	//	
+	
 struct file_operations gpumem_fops = {
 
     .owner = THIS_MODULE,
@@ -162,8 +207,6 @@ struct file_operations gpumem_fops = {
     .mmap = v2p2v_mmap,
 };
 
-//-----------------------------------------------------------------------------
-
 static struct miscdevice gpumem_dev = {
 
     MISC_DYNAMIC_MINOR,
@@ -171,23 +214,17 @@ static struct miscdevice gpumem_dev = {
     &gpumem_fops
 };
 
-//-----------------------------------------------------------------------------
-
+// for module initialization and clean up	//
 static int __init gpumem_init(void)
 {
     pr_info(V2P2V_DRIVER_NAME ": %s()\n", __func__);
     dev.proc = 0;
     sema_init(&dev.sem, 1);
     INIT_LIST_HEAD(&dev.table_list);
-    //zyuxuan
     gpumem_register_proc(V2P2V_DRIVER_NAME, 0, &dev);
-    //gpumem_register_proc("Catapult FPGA", 0, &dev);
-
     misc_register(&gpumem_dev);
     return 0;
 }
-
-//-----------------------------------------------------------------------------
 
 static void __exit gpumem_cleanup(void)
 {
@@ -196,9 +233,6 @@ static void __exit gpumem_cleanup(void)
     misc_deregister(&gpumem_dev);
 }
 
-//-----------------------------------------------------------------------------
-
 module_init(gpumem_init);
 module_exit(gpumem_cleanup);
 
-//-----------------------------------------------------------------------------
